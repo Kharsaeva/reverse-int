84292f0276440fa9a92451b214c4590bf03382b6
Aisha <arsaevaaisha@gmail.com>
2023-02-28 01:50:52 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let reversed = 0;
+
+  while (number !== 0) {
+    const digit = number % 10;
+    reversed = reversed * 10 + digit;
+    number = Math.floor(number / 10);
+  }
+
+  return reversed;
 }
